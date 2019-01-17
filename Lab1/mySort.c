bee4b157d984d5192aa2b2a96c28987afd8ebc15
BrandonMxstery <39285783+BrandonMxstery@users.noreply.github.com>
2019-01-17 17:31:57 -0500

    Add files via upload

---
+++ Lab1/mySort.c
@@ -0,0 +1,25 @@
+void mySort(int d[], unsigned int n)
+{
+	int i;
+	int j;
+	int min;
+	int swap;
+	for(i=0; i < n-1; i++){
+		min = i;
+		for (j=i+1; j<n;j++){
+			if (d[j] < d[min]){
+				min = j;
+			}
+}
+		if (min != i)	{
+			swap = d[min];
+			d[min] = d[i];
+			d[i]=swap;
+
+		
+
+}
+}
+}
+
+
