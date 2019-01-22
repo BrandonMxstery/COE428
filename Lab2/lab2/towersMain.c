e7dd663cd760592b6d1b6f3b51e5d6ca620c6b6c
Brandon <39285783+T1Brandon@users.noreply.github.com>
2019-01-22 18:12:36 -0500

    Add files via upload

--- Lab2/lab2/towersMain.c
+++ Lab2/lab2/towersMain.c
@@ -8,6 +8,13 @@ int main(int argc, char **argv)
     int n = 3;
     int from = 1;
     int dest = 2;
+    if (argc == 1) {
+	int n = 3;
+        int from = 1;
+        int dest = 2;
+	towers(n, from, dest);
+        exit(0);
+    }
     if (argc == 2) {
         n = atoi(argv[1]);
 	towers(n, from, dest);
