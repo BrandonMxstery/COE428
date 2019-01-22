d7ad143ba4d9853cf563a7a0cd267412b013a263
Brandon <39285783+T1Brandon@users.noreply.github.com>
2019-01-22 18:14:23 -0500

    Update towersMain.c

--- Lab2/lab2/towersMain.c
+++ Lab2/lab2/towersMain.c
@@ -9,9 +9,6 @@ int main(int argc, char **argv)
     int from = 1;
     int dest = 2;
     if (argc == 1) {
-	int n = 3;
-        int from = 1;
-        int dest = 2;
 	towers(n, from, dest);
         exit(0);
     }
