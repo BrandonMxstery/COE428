09ace282326c55f877e9835dc7f5e83789396b3f
Brandon <39285783+T1Brandon@users.noreply.github.com>
2019-01-27 14:58:18 -0500

    Update towersMain.c

--- Lab2/lab2/towersMain.c
+++ Lab2/lab2/towersMain.c
@@ -5,7 +5,6 @@
 
 int main(int argc, char **argv)
 {
-  if (argv[1] => 0 and argv[2] => 0 and argv[3] =>0){
     int n = 3;
     int from = 1;
     int dest = 2;
@@ -13,22 +12,21 @@ int main(int argc, char **argv)
 	towers(n, from, dest);
         exit(0);
     }
-    if (argc == 2) {
+    if (argc == 2 && atoi(argv[1]) >= 0){
         n = atoi(argv[1]);
 	towers(n, from, dest);
         exit(0);
     }
-    if (argc == 4){
+    if (argc == 4 && atoi(argv[1]) >= 0 && atoi(argv[2]) >= 0 && atoi(argv[3]) >= 0){
 	n = atoi(argv[1]);
 	from = atoi(argv[2]);
 	dest = atoi(argv[3]);
-        towers(n, from, dest);
+	towers(n, from, dest);
         exit(0);
     }
     else{
     	fprintf(stderr,"Wrong Input");
     	exit(1);
-}
 }
 }
 
